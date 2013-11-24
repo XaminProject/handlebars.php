93106632ebd9e3f446724351459a1f52724de123
Mardix <mcx2082@gmail.com>
2013-11-24 13:44:51 -0500

    2.0 - Added else in {{#each}} expression. Added  slice array in {{#each Array[start:end]}}. New Built in helpers for string manipulation. Plus cleanup

--- src/Handlebars/Helpers.php
+++ src/Handlebars/Helpers.php
@@ -3,14 +3,20 @@
  * This file is part of Handlebars-php
  * Base on mustache-php https://github.com/bobthecow/mustache.php
  *
- * PHP version 5.3
+ * Handlebars helpers
+ *
+ * a collection of helper function. normally a function like
+ * function ($sender, $name, $arguments) $arguments is unscaped arguments and
+ * is a string, not array
  *
  * @category  Xamin
  * @package   Handlebars
  * @author    fzerorubigd <fzerorubigd@gmail.com>
  * @author    Behrooz Shabani <everplays@gmail.com>
+ * @author    Mardix <https://github.com/mardix>
  * @copyright 2012 (c) ParsPooyesh Co
  * @copyright 2013 (c) Behrooz Shabani
+ * @copyright 2013 (c) Mardix
  * @license   MIT <http://opensource.org/licenses/MIT>
  * @version   GIT: $Id$
  * @link      http://xamin.ir
@@ -19,32 +25,16 @@
 namespace Handlebars;
 
 use DateTime;
-
-/**
- * Handlebars helpers
- *
- * a collection of helper function. normally a function like
- * function ($sender, $name, $arguments) $arguments is unscaped arguments and
- * is a string, not array
- *
- * TODO: Add support for an interface with an execute method
- *
- * @category  Xamin
- * @package   Handlebars
- * @author    fzerorubigd <fzerorubigd@gmail.com>
- * @copyright 2012 (c) ParsPooyesh Co
- * @license   MIT <http://opensource.org/licenses/MIT>
- * @version   Release: @package_version@
- * @link      http://xamin.ir
- */
+use InvalidArgumentException;
+use Traversable;
 
 class Helpers
 {
     /**
      * @var array array of helpers
      */
-    protected $helpers = array();
-    protected $defaultHelpers = [
+    protected $helpers = [];
+    protected $builtinHelpers = [
         "if", 
         "each",
         "with",
@@ -53,34 +43,32 @@ class Helpers
         "upper",
         "lower",
         "capitalize",
-        "capitalizeWords",
+        "capitalize_words",
         "reverse",
-        "formatDate",
+        "format_date",
         "inflect",
         "default",
         "truncate",
+        "raw"
     ];
 
     /**
      * Create new helper container class
      *
      * @param array      $helpers  array of name=>$value helpers
-     * @param array|bool $loadDefault add defaults helper (if, unless, each,with, bindAttr)
-     *
      * @throws \InvalidArgumentException when $helpers is not an array
      * (or traversable) or helper is not a callable
      */
-    public function __construct($helpers = null, $loadDefault = true)
+    public function __construct($helpers = null)
     {
-        if ($loadDefault) {
-            foreach($this->defaultHelpers as $helper) {
-                $this->add($helper, [$this, "helper{$helper}"]);
-            }
+        foreach($this->builtinHelpers as $helper) {
+            $helperName = $this->underscoreToCamelCase($helper);
+            $this->add($helper, [$this, "helper{$helperName}"]);
         }
 
         if ($helpers != null) {
-            if (!is_array($helpers) && !$helpers instanceof \Traversable) {
-                throw new \InvalidArgumentException(
+            if (!is_array($helpers) && !$helpers instanceof Traversable) {
+                throw new InvalidArgumentException(
                     'HelperCollection constructor expects an array of helpers'
                 );
             }
@@ -102,7 +90,7 @@ class Helpers
     public function add($name, $helper)
     {
         if (!is_callable($helper)) {
-            throw new \InvalidArgumentException("$name Helper is not a callable.");
+            throw new InvalidArgumentException("$name Helper is not a callable.");
         }
         $this->helpers[$name] = $helper;
     }
@@ -130,7 +118,7 @@ class Helpers
     public function __get($name)
     {
         if (!$this->has($name)) {
-            throw new \InvalidArgumentException('Unknown helper :' . $name);
+            throw new InvalidArgumentException('Unknown helper :' . $name);
         }
         return $this->helpers[$name];
     }
@@ -166,7 +154,6 @@ class Helpers
      * Unset a helper
      *
      * @param string $name helper name to remove
-     *
      * @return void
      */
     public function __unset($name)
@@ -178,14 +165,13 @@ class Helpers
      * Check whether a given helper is present in the collection.
      *
      * @param string $name helper name
-     *
      * @throws \InvalidArgumentException if the requested helper is not present.
      * @return void
      */
     public function remove($name)
     {
         if (!$this->has($name)) {
-            throw new \InvalidArgumentException('Unknown helper: ' . $name);
+            throw new InvalidArgumentException('Unknown helper: ' . $name);
         }
         unset($this->helpers[$name]);
     }
@@ -199,7 +185,7 @@ class Helpers
      */
     public function clear()
     {
-        $this->helpers = array();
+        $this->helpers = [];
     }
 
     /**
@@ -229,25 +215,28 @@ class Helpers
     public function helperIf($template, $context, $args, $source)
     {
         $tmp = $context->get($args);
-        if ($tmp) {
+        if ($tmp) {            
             $template->setStopToken('else');
             $buffer = $template->render($context);
             $template->setStopToken(false);
-            $template->discard($context);
+            $template->discard();
+            return $buffer;
         } else {
-            $template->setStopToken('else');
-            $template->discard($context);
-            $template->setStopToken(false);
-            $buffer = $template->render($context);
+            return $this->renderElse($template, $context);
         }
-        return $buffer;
     }
+
 
     /**
      * Create handler for the 'each' helper.
      * example {{#each people}} {{name}} {{/each}}
      * example with slice: {{#each people[0:10]}} {{name}} {{/each}}
-     * 
+     * example with else
+     *  {{#each Array}}
+     *        {{.}}
+     *  {{else}}
+     *      Nothing found
+     *  {{/each}}
      *
      * @param \Handlebars\Template $template template that is being rendered
      * @param \Handlebars\Context  $context  context object
@@ -261,62 +250,57 @@ class Helpers
     {
         list($keyname, $slice_start, $slice_end) = $this->extractSlice($args);
         $tmp = $context->get($keyname);
-        $buffer = '';
-        if (is_array($tmp) || $tmp instanceof \Traversable) {
+    
+        if (is_array($tmp) || $tmp instanceof Traversable) {
             $tmp = array_slice($tmp, $slice_start, $slice_end);
-
+            $buffer = '';
             $islist = (array_keys($tmp) == range(0, count($tmp) - 1));
-
-            foreach ($tmp as $key => $var) {
-                if ($islist) {
-                    $context->pushIndex($key);
-                } else {
-                    $context->pushKey($key);
+            
+            if (is_array($tmp) && ! count($tmp)) {
+                return $this->renderElse($template, $context);
+            } else {
+                foreach ($tmp as $key => $var) {                
+                    $tpl = clone $template;
+                    if ($islist) {
+                        $context->pushIndex($key);
+                    } else {
+                        $context->pushKey($key);
+                    }
+                    $context->push($var);
+                    $tpl->setStopToken('else');
+                    $buffer .= $tpl->render($context);
+                    $context->pop();
+                    if ($islist) {
+                        $context->popIndex();
+                    } else {
+                        $context->popKey();
+                    }
                 }
-                $context->push($var);
-                $buffer .= $template->render($context);
-                $context->pop();
-                if ($islist) {
-                    $context->popIndex();
-                } else {
-                    $context->popKey();
-                }
+                return $buffer;
             }
+        } else {
+            return $this->renderElse($template, $context);
         }
-        return $buffer;
     }
-
+    
     /**
-     * slice
-     * Allow to split the data that will be returned
-     * #loop[start:end] => starts at start trhough end -1
-     * #loop[start:] = Starts at start though the rest of the array
-     * #loop[:end] = Starts at the beginning through end -1
-     * #loop[:] = A copy of the whole array
-     * 
-     * #loop[-1]
-     * #loop[-2:] = Last two items
-     * #loop[:-2] = Everything except last two items
-     * 
-     * @param string $string
-     * @return Array [tag_name, slice_start, slice_end]
-     */    
-    protected function extractSlice($string) 
+     * Applying the DRY principle here.
+     * This method help us render {{else}} portion of a block
+     * @param \Handlebars\Template $template
+     * @param \Handlebars\Context $context
+     * @return string
+     */
+    private function renderElse($template, $context)
     {
-        preg_match("/^([\w\._\-]+)(?:\[([\-0-9]*?:[\-0-9]*?)\])?/i", $string, $m);  
-        $slice_start = $slice_end = null;
-        if (isset($m[2])) {
-            list($slice_start, $slice_end) = explode(":", $m[2]);
-            $slice_start = (int) $slice_start;
-            $slice_end = $slice_end ? (int) $slice_end : null;
-        } 
-        return [$m[1], $slice_start, $slice_end];
-    } 
+        $template->setStopToken('else');
+        $template->discard();
+        $template->setStopToken(false);
+        return $template->render($context);        
+    }
+    
     
     /**
      * Create handler for the 'unless' helper.
-     * Needed for compatibility with PHP 5.2 since it doesn't support anonymous
-     * functions.
      *
      * @param \Handlebars\Template $template template that is being rendered
      * @param \Handlebars\Context  $context  context object
@@ -459,7 +443,7 @@ class Helpers
     
     /**
      * Format a date
-     * {{#formatDate date 'Y-m-d @h:i:s'}}
+     * {{#format_date date 'Y-m-d @h:i:s'}}
      * @param \Handlebars\Template $template template that is being rendered
      * @param \Handlebars\Context  $context  context object
      * @param array                $args     passed arguments to helper
@@ -533,8 +517,8 @@ class Helpers
     }
     
    /**
-     * Provide a default fallback
-     * {{default content 5 "..."}} 
+     * Truncate a string to a length, and append and ellipsis if provided
+     * {{#truncate content 5 "..."}} 
      * 
      *
      * @param \Handlebars\Template $template template that is being rendered
@@ -550,11 +534,65 @@ class Helpers
         preg_match("/(.*?)\s+(.*?)\s+(?:(?:\"|\')(.*?)(?:\"|\'))/", trim($args), $m);
         $keyname = $m[1];
         $limit = $m[2];
-        $elipse = $m[3];
+        $ellipsis = $m[3];
         $value = substr($context->get($keyname), 0, $limit);
-        if ($elipse) {
-            $value .= $elipse;
+        if ($ellipsis) {
+            $value .= $ellipsis;
         }
         return $value;
     }    
+    
+    /**
+     * {{#raw}} {{/raw}}
+     *
+     * @param \Handlebars\Template $template template that is being rendered
+     * @param \Handlebars\Context  $context  context object
+     * @param array                $args     passed arguments to helper
+     * @param string               $source   part of template that is wrapped
+     *                                       within helper
+     *
+     * @return mixed
+     */
+    public function helperRaw($template, $context, $args, $source)
+    {
+        return $source;
+    }
+    
+    /**
+     * Change underscore helper name to CamelCase
+     * 
+     * @param string $string
+     * @return string
+     */
+    private function underscoreToCamelCase($string)
+    {
+        return str_replace(' ', '', ucwords(str_replace('_', ' ', $string)));
+    } 
+    
+    /**
+     * slice
+     * Allow to split the data that will be returned
+     * #loop[start:end] => starts at start trhough end -1
+     * #loop[start:] = Starts at start though the rest of the array
+     * #loop[:end] = Starts at the beginning through end -1
+     * #loop[:] = A copy of the whole array
+     * 
+     * #loop[-1]
+     * #loop[-2:] = Last two items
+     * #loop[:-2] = Everything except last two items
+     * 
+     * @param string $string
+     * @return Array [tag_name, slice_start, slice_end]
+     */    
+    private function extractSlice($string) 
+    {
+        preg_match("/^([\w\._\-]+)(?:\[([\-0-9]*?:[\-0-9]*?)\])?/i", $string, $m);  
+        $slice_start = $slice_end = null;
+        if (isset($m[2])) {
+            list($slice_start, $slice_end) = explode(":", $m[2]);
+            $slice_start = (int) $slice_start;
+            $slice_end = $slice_end ? (int) $slice_end : null;
+        } 
+        return [$m[1], $slice_start, $slice_end];
+    }     
 }
