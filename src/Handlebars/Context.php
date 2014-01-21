7894899aba632e9cbe9c18f0be06e89a8f1e3ddd
ulriklystbaek <ulriklystbaek@gmail.com>
2014-01-21 09:42:48 +0100

    Update Context.php
    Added author

--- src/Handlebars/Context.php
+++ src/Handlebars/Context.php
@@ -10,6 +10,7 @@
  * @author    fzerorubigd <fzerorubigd@gmail.com>
  * @author    Behrooz Shabani <everplays@gmail.com>
  * @author    Chris Gray <chris.w.gray@gmail.com>
+ * @author    Ulrik Lystbaek <ulrik@bettertaste.dk>
  * @copyright 2012 (c) ParsPooyesh Co
  * @copyright 2013 (c) Behrooz Shabani
  * @copyright 2013 (c) f0ruD A
