ae511f20606de04e16e8a0d727828ba86fdb1a70
Ehsanul Haque <ehsanul@featurecreeptechnology.com>
2014-08-01 14:22:03 -0400

    Fix for failing cache clear

--- src/Handlebars/Loader/FilesystemLoader.php
+++ src/Handlebars/Loader/FilesystemLoader.php
@@ -103,11 +103,11 @@ class FilesystemLoader implements Loader
      */
     public function load($name)
     {
-        if (!isset($this->_templates[$name])) {
-            $this->_templates[$name] = $this->loadFile($name);
+        if (empty($this->_templates) || !isset($this->_templates["$name"])) {
+            $this->_templates["$name"] = $this->loadFile($name);
         }
 
-        return new String($this->_templates[$name]);
+        return new String($this->_templates["$name"]);
     }
 
     /**
