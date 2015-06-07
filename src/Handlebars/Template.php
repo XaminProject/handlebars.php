23076d68e5339640cab7b748dc9834f160c09738
Martin Zeman <me@zemistr.eu>
2015-06-07 23:09:14 +0200

    Added support for class with __toString method as a context variable.

--- src/Handlebars/Template.php
+++ src/Handlebars/Template.php
@@ -591,14 +591,14 @@ class Template
         }
         if ($escaped) {
             $args = $this->handlebars->getEscapeArgs();
-            array_unshift($args, $value);
+            array_unshift($args, (string)$value);
             $value = call_user_func_array(
                 $this->handlebars->getEscape(),
                 array_values($args)
             );
         }
 
-        return $value;
+        return (string)$value;
     }
 
     /**
