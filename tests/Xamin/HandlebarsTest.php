198faa48d620807541dacf84fe03a785692f1203
behrooz shabani (everplays) <everplays@gmail.com>
2016-07-31 12:31:23 +0200

    more php 5.3 syntax fixes

--- tests/Xamin/HandlebarsTest.php
+++ tests/Xamin/HandlebarsTest.php
@@ -1387,15 +1387,15 @@ EOM;
     {
         return array(array(
             '{{#with b}}{{#if this}}{{../a}}{{/if}}{{/with}}',
-            ['a' => 'good', 'b' => 'stump'],
+            array('a' => 'good', 'b' => 'stump'),
             'good',
         ), array(
             '{{#with b}}{{#unless false}}{{../a}}{{/unless}}{{/with}}',
-            ['a' => 'good', 'b' => 'stump'],
+            array('a' => 'good', 'b' => 'stump'),
             'good',
         ), array(
             '{{#with foo}}{{#if goodbye}}GOODBYE cruel {{../world}}!{{/if}}{{/with}}',
-            ['foo' => ['goodbye' => true], 'world' => 'world'],
+            array('foo' => array('goodbye' => true), 'world' => 'world'),
             'GOODBYE cruel world!',
         ));
     }
